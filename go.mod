00b32167f07dbb3cd771e7b763c14e4e7d4be033
andvarfolomeev <andreika.varfolomeev@yandex.ru>
2025-06-17 18:40:51 +0300

    refactor: simplifying

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module andvarfolomeev/go-scan
+module github.com/andvarfolomeev/go-scan
 
 go 1.22.2
--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"andvarfolomeev/go-scan/scanner"
 	"flag"
 	"fmt"
 	"log/slog"
+	"net"
 	"os"
+	"sort"
+	"sync"
 	"time"
 )
+
+type ScanHostOptions struct {
+	Host    string
+	Workers int
+	To      int
+	From    int
+	Timeout time.Duration
+}
 
 func main() {
 	var debugFlag = flag.Bool("debug", false, "debug")
@@ -25,11 +35,59 @@ func main() {
 	}
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: logLevel})))
 
-	fmt.Printf("Opened ports: %v\n", scanner.ScanHost(&scanner.ScanHostOptions{
+	fmt.Printf("Opened ports: %v\n", ScanHost(&ScanHostOptions{
 		Host:    *host,
 		Workers: *workers,
 		To:      *to,
 		From:    *from,
 		Timeout: 200 * time.Millisecond,
 	}))
+}
+
+func scanHostWorker(opt *ScanHostOptions, id int, portsCh chan int, resultsCh chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for p := range portsCh {
+		address := fmt.Sprintf("%s:%d", opt.Host, p)
+		slog.Debug("scanning", "workerId", id, "port", p)
+		conn, err := net.DialTimeout("tcp", address, opt.Timeout)
+		if err == nil {
+			slog.Debug("close", "workerId", id, "port", p)
+			conn.Close()
+			resultsCh <- p
+		}
+	}
+}
+
+func ScanHost(opt *ScanHostOptions) []int {
+	portsCh := make(chan int, opt.Workers)
+	resultsCh := make(chan int)
+
+	var wg sync.WaitGroup
+
+	for id := range opt.Workers {
+		wg.Add(1)
+		go scanHostWorker(opt, id, portsCh, resultsCh, &wg)
+	}
+
+	go func() {
+		for i := opt.From; i <= opt.To; i++ {
+			portsCh <- i
+		}
+		close(portsCh)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(resultsCh)
+	}()
+
+	var openedPorts []int
+
+	for p := range resultsCh {
+		openedPorts = append(openedPorts, p)
+	}
+
+	sort.Ints(openedPorts)
+	return openedPorts
 }
